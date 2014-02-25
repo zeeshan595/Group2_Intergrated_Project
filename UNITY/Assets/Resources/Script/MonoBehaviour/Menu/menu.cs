02e707a0b6492a9e0cc60864b89ffed43116963e
Zeeshan Abid <zeeshan595@googlemail.com>
2014-02-25 21:55:20 +0000

    Adventure car added

---
+++ UNITY/Assets/Resources/Script/MonoBehaviour/Menu/menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour
+{
+    public GameObject action;
+    public GameObject cyberpunk;
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Action Car"))
+        {
+            Instantiate(action, new Vector3(0, 1, 0), Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if (GUILayout.Button("Cyberpunk Car"))
+        {
+            Instantiate(cyberpunk, new Vector3(0, 1, 0), Quaternion.identity);
+            Destroy(gameObject);
+        }
+        GUILayout.Label("________________\n\n");
+        for (int x = 0; x < Settings.buttons.Length; x++)
+        {
+            GUILayout.Label(Settings.buttons[x].name + ": " + Settings.buttons[x].key);
+        }
+    }
+}
