e400cf21f11a096e42bcb72d04e44d5e998e9227
Zeeshan Abid <zeeshan595@googlemail.com>
2014-03-12 23:53:58 +0000

    Elevator Added

---
+++ UNITY/Assets/Resources/Script/MonoBehaviour/Menu/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Action"))
+        {
+            Settings.carType = Settings.CarType.Action;
+            Application.LoadLevel("Tutorial");
+        }
+
+        if (GUILayout.Button("Romance"))
+        {
+            Settings.carType = Settings.CarType.Romance;
+            Application.LoadLevel("Tutorial");
+        }
+
+        if (GUILayout.Button("Fantasy"))
+        {
+            Settings.carType = Settings.CarType.Fantasy;
+            Application.LoadLevel("Tutorial");
+        }
+    }
+}
