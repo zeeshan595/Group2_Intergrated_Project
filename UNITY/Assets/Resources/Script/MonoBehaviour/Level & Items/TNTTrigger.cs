79290570d68bdd8ca5ce28ebbccc6765c7cf5930
Zeeshan Abid <zeeshan595@googlemail.com>
2014-04-01 16:35:55 +0100

    Level1 Update

---
+++ UNITY/Assets/Resources/Script/MonoBehaviour/Level & Items/TNTTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TNTTrigger : MonoBehaviour
+{
+    public GameObject TNT;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent.transform.rigidbody.AddForce(new Vector3(1, 0.3f, 0) * 1000);
+            Instantiate(TNT.GetComponent<TNT>().explosion, TNT.transform.position, Quaternion.identity);
+            Destroy(TNT);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ UNITY/Assets/Resources/Script/MonoBehaviour/Level & Items/TrapDoor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoor : MonoBehaviour
+{
+    public GameObject trap;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            trap.GetComponent<HingeJoint>().useSpring = false;
+        }
+    }
+}
