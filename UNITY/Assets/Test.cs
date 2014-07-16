89101c1fc4ea01bad3cf3ed03a42a167c743f801
Zeeshan Abid <zeeshan595@googlemail.com>
2014-07-17 00:48:07 +0100

    Just Deleting some stuff

--- UNITY/Assets/Test.cs
+++ UNITY/Assets/Test.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+    private bool isTrue = false;
+
 	// Use this for initialization
 	void Start () {
-	
+        Debug.Log("test");
 	}
 	
 	// Update is called once per frame
